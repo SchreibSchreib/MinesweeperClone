e7781c2d7305f92ef29fe1c38e476e9b4e7a151b
Darth <Darth@DARTH-PC>
2023-03-22 16:18:57 +0100

    Projektdateien hinzufügen.

---
+++ Minesweeper/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_X_Direction_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox? boxClicked = sender as TextBox;
+            if (boxClicked?.Name == "TextBox_Y_Direction")
+            {
+                if (boxClicked.Text == "Felder Y")
+                {
+                    boxClicked.Text = null;
+                }
+            }
+            else
+            {
+                if (boxClicked?.Text == "Felder X")
+                {
+                    boxClicked.Text = null;
+                }
+            }
+        }
+
+        private void TextBox_X_Direction_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox? boxLostFocus = sender as TextBox;
+#pragma warning disable CS8602 // Dereferenzierung eines möglichen Nullverweises.
+            if (boxLostFocus.Text.Contains(" ") || boxLostFocus.Text.Length == 0 || boxLostFocus.Text == null || Convert.ToInt32(boxLostFocus.Text) < 10 || Convert.ToInt32(boxLostFocus.Text) > 50)
+            {
+                if (boxLostFocus.Name == "TextBox_Y_Direction")
+                {
+                    if ((boxLostFocus.Text.Contains(" ") || boxLostFocus.Text.Length == 0 || boxLostFocus.Text == null || Convert.ToInt32(boxLostFocus.Text) < 10 || Convert.ToInt32(boxLostFocus.Text) > 50 || boxLostFocus.Text.Length == 0 || boxLostFocus.Text == null))
+                    { 
+                        MessageBox.Show("Aktuell werden nur Zahlen zwischen 10 und 50 supportet."); 
+                    }
+                    boxLostFocus.Text = "Felder Y";
+                }
+                else
+                {
+#pragma warning disable CS8602 // Dereferenzierung eines möglichen Nullverweises.
+                    if (boxLostFocus.Text.Contains(' ') || boxLostFocus.Text.Length == 0 || boxLostFocus.Text == null || Convert.ToInt32(boxLostFocus.Text) < 10 || Convert.ToInt32(boxLostFocus.Text) > 50)
+                    {
+                        MessageBox.Show("Aktuell werden nur Zahlen zwischen 10 und 50 supportet.");
+                    }
+#pragma warning restore CS8602 // Dereferenzierung eines möglichen Nullverweises.
+                    boxLostFocus.Text = "Felder X";
+                }
+            }
+#pragma warning restore CS8602 // Dereferenzierung eines möglichen Nullverweises.
+        }
+        private void NumberValidation(object sender, TextCompositionEventArgs e) 
+        {
+            Regex checkOnlyNumbers = new Regex("[^0-9]+");
+            e.Handled = checkOnlyNumbers.IsMatch(e.Text);
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            Field startField = new Field(Convert.ToInt32(TextBox_X_Direction.Text),Convert.ToInt32(TextBox_Y_Direction.Text));
+            List<Playbutton> playButtons= new List<Playbutton>();
+            foreach (KeyValuePair<string,bool> fieldKey in startField.playGround)
+            {
+                playButtons.Add(new Playbutton(fieldKey));
+            }
+            Spielbrett newSession = new Spielbrett(playButtons, Convert.ToInt32(TextBox_X_Direction.Text), Convert.ToInt32(TextBox_Y_Direction.Text));
+            Playbutton.currentSession = newSession;
+            newSession.Show();
+        }
+
+        private void leaderBoardButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Minesweeper/Spielbrett.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// Interaktionslogik für Spielbrett.xaml
+    /// </summary>
+    public partial class Spielbrett : Window
+    {
+        public Spielbrett(List<Playbutton> playButtons,int buttonsX,int buttonsY)
+        {
+            this.buttonsX = buttonsX;
+            this.buttonsY = buttonsY;
+            this.buttons = playButtons;
+            InitializeComponent();
+        }
+        private List<Playbutton> buttons;
+        private int buttonsX;
+        private int buttonsY;
+
+        private void SpawnGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int row = 0; row < buttonsY; row++)
+            {
+                SpawnGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int column = 0; column < buttonsX; column++)
+            {
+                SpawnGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            foreach (Playbutton button in buttons)
+            {
+                SpawnGrid.Children.Add(button);
+                Grid.SetRow(button, Convert.ToInt32(button.coordinates.Split(' ')[0]));
+                Grid.SetColumn(button, Convert.ToInt32(button.coordinates.Split(' ')[1]));
+            }
+           
+        }
+    }
+}
